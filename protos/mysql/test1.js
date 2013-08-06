bbe03811f843af45f3df6473fc4a590f15021076
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-08-07 01:58:05 +0300

    Implemented MySQL

--- protos/mysql/test1.js
+++ protos/mysql/test1.js
@@ -1,6 +1,7 @@
 /* MySQL proto test */
 
-db({'user':'jhh'}).begin().select({from:'foo', limit:1}).then(function(data) {
+var DB = require('../../lib/index.js');
+DB({'user':'jhh'}).begin().select({from:'foo', limit:1}).then(function(data) {
 	return this.update('bar', {'set':data.shift()});
 }).commit().fail(function(err) {
 	console.error('Error: ' + err);
