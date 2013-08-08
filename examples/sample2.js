c9fe84ff2c9a0ebdb0da14a0581d21f847929985
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-08-08 03:48:34 +0300

    Updated

---
+++ examples/sample2.js
@@ -0,0 +1,12 @@
+/* Sample usage for node-nor-db */
+
+var db = new require('../lib/index.js').MySQL({'database':'test'});
+
+db.connect().select({id:10}).then(function(rows) {
+	console.log("rows = " + JSON.stringify(rows) );
+}).end().fail(function(err) {
+	console.error('ERROR: ' + err);
+	if(err.stack) console.error(err.stack);
+}).done();
+
+/* EOF */
